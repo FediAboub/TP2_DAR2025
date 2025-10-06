6bfb076e2eb7bbbcfa64fcc725b18d80bc8cb355
FediAboub <fedi.aboub2002@gmail.com>
2025-10-06 22:21:12 +0100

    Mon premier projet

---
+++ eclipse-workspace/TP1_DAR2025/src/serverPackage/Server.java
@@ -0,0 +1,8 @@
+package serverPackage;
+
+public class Server {
+	public static void main(String[]args) {
+	System.out.println("je suis un serveur en attente a la connexuion d un client");
+	System.out.println(" un client connecté");
+	}
+}
